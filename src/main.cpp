9cbe3376cd08ad0b16defeca616d29351e198fc9
Anthony Cruz <cruzanta@outlook.com>
2018-04-08 04:38:48 -0400

    Add loop to allow user to enter more height/weight combinations

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,46 @@ int main()
     // Initialize variables for the introduction box
     int introBoxWid = 54;
     std::string appName = "Body Mass Index Calculator";
+    // Initialize variable for the control loop
+    std::string userResume;
 
-    // Display the introduction box with the application name
-    std::cout << "\n"
-              << std::string(introBoxWid, '*') << "\n"
-              //<< std::left
-              << std::setw(1) << "*"
-              << std::setw((introBoxWid - appName.length() - 1) / 2) << ""
-              << std::setw(appName.length()) << appName
-              << std::setw((introBoxWid - appName.length() - 1) / 2) << ""
-              << std::setw(1) << "*" << "\n"
-              << std::string(introBoxWid, '*') << "\n"
-              << "\n";
+    while (userResume != "n")
+    {
+        // Display the introduction box with the application name
+        std::cout << "\n"
+                  << std::string(introBoxWid, '*') << "\n"
+                  << std::setw(1) << "*"
+                  << std::setw((introBoxWid - appName.length() - 1) / 2) << ""
+                  << std::setw(appName.length()) << appName
+                  << std::setw((introBoxWid - appName.length() - 1) / 2) << ""
+                  << std::setw(1) << "*" << "\n"
+                  << std::string(introBoxWid, '*') << "\n"
+                  << "\n";
 
-    // Prompt the user for their height and weight
-    std::cout << "Your Height (inches): ";
-    std::cin >> height;
-    std::cout << "Your Weight (pounds): ";
-    std::cin >> weight;
+        // Prompt the user for their height and weight
+        std::cout << "Your Height (inches): ";
+        std::cin >> height;
+        std::cout << "Your Weight (pounds): ";
+        std::cin >> weight;
 
-    // Initialize a Person object
-    Person user(height, weight);
+        // Initialize a Person object
+        Person user(height, weight);
 
-    // Display the BMI report
-    std::cout << "\n";
-    displayBodyMassIndexReport(user.getHeight(), user.getWeight());
-    std::cout << "\n" << "\n";
+        // Display the BMI report
+        std::cout << "\n";
+        displayBodyMassIndexReport(user.getHeight(), user.getWeight());
+        std::cout << "\n" << "\n";
+
+        // Ask the user if they would like to continue
+        std::cout << "Would you like to try a different height/weight? (y/n): ";
+        std::cin >> userResume;
+
+        while (userResume != "y" && userResume != "n")
+        {
+            std::cout << "Error: Please enter 'y' or 'n': ";
+            std::cin >> userResume;
+        }
+    }
 
     return 0;
 }
